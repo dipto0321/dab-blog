265e8139825ab319b27373102495ef00471f998a
dipto0321 <diptokmk47@gmail.com>
2018-11-29 21:13:48 +0600

    Initial model task with test
        * Add test file for validate model data
        * Ad some validation on user model
        * Add bycript for secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :fullname, presence: true
+  validates :username, presence: true
+  validates :email, presence: true
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {described_class.new}
+  it "is valid with valid attributes" do
+    subject.fullname = 'John Doe'
+    subject.username = 'john01'
+    subject.email = 'john@demo.com'
+    subject.password = 'pa$$0rd'
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a fullname" do
+    user = User.new(fullname: nil)
+    expect(user).to_not be_valid
+  end
+  it "is not valid without a username"
+  it "should have a uniqe username"
+  it "is not valid without a email"
+  it "should have a uniqe email"
+  it "is not valid without a password"
+  describe 'password' do
+    it "is not valid without minimum six characters"
+    it "is not valid without atleast one number and special character"
+  end
 end
