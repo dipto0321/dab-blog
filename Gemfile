ac420f8245f4782ec0571d0035a6a7fa2f2e7fa0
dipto0321 <diptokmk47@gmail.com>
2018-11-30 21:34:42 +0600

    Task done for user model
        * Add validations for all data
        * Add test for these models
        * Need to be add test for uniqeness  check

--- Gemfile
+++ Gemfile
@@ -34,6 +34,11 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'devise', '~> 4.5'
+gem 'bootstrap', '~> 4.1', '>= 4.1.3'
+gem 'font-awesome-sass', '~> 5.5', '>= 5.5.0.1'
+gem 'jquery-rails'
+gem 'simple_form'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  VALID_PASSWORD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/.freeze
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :fullname, presence: true
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
   has_secure_password
+  validates :password, presence: true,
+                       format: { with: VALID_PASSWORD_REGEX },
+                       length: { minimum: 6 }
 end
