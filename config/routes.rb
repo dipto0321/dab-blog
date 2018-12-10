ea9cc061e183fda7374b8904ce46efafa72acdea
dipto0321 <diptokmk47@gmail.com>
2018-12-10 11:58:35 +0600

    Initial post controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
   get 'users/index'
   get 'users/show'
   get 'users/new'
